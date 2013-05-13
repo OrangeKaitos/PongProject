433cd258edac7a4365e232395ffd4dff1e983a8f
Philip <pstiff@live.se>
2013-05-13 14:48:33 +0200

    Buggfix

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,45 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
-  
+
 public class Main extends StateBasedGame {
-	
-	 public static final int MAINMENUSTATE          = 0;
-	 public static final int GAMESINGLESTATE        = 1;
-	 public static final int GAMEMULTISTATE			= 2;
-	 public static final int GAMEOVERSTATE			= 3;
-	 public static final int INSTRUCTIONSTATE		= 4;
-	 public static final int HIGHSCORESTATE			= 5;
-	 public static final int screenX						= 800;
-	 public static final int screenY 						= 400;
+
+	public static final int MAINMENUSTATE = 0;
+	public static final int GAMESINGLESTATE = 1;
+	public static final int GAMEMULTISTATE = 2;
+	public static final int GAMEOVERSTATE = 3;
+	public static final int INSTRUCTIONSTATE = 4;
+	public static final int HIGHSCORESTATE = 5;
+	public static final int screenX = 800;
+	public static final int screenY = 600;
 
 	public Main() {
 		super("Pong");
 	}
 
-	public static void main(String[] args) throws SlickException
-    {
-         AppGameContainer app = new AppGameContainer(new Pong());
-  
-         app.setDisplayMode(screenX, screenY, false);
-         app.start();
-    }
-  
-    @Override
-    public void initStatesList(GameContainer gameContainer) throws SlickException {
-        this.addState(new MainMenuState(MAINMENUSTATE));
-        this.addState(new GamePlayState(GAMESINGLESTATE));
-        this.addState(new GamePlayState(GAMEMULTISTATE));
-        this.addState(new GameOverState(GAMEOVERSTATE));
-        this.addState(new HighscoreState(HIGHSCORESTATE));
-    }
-    
-    public static int screenX() {
-    	return screenX;
-    }
-    public static int screenY() {
-    	return screenY;
-    }
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new Main());
+
+		app.setDisplayMode(screenX, screenY, false);
+		app.start();
+	}
+
+	@Override
+	public void initStatesList(GameContainer gameContainer)
+			throws SlickException {
+		this.addState(new MainMenuState(MAINMENUSTATE));
+		this.addState(new GamePlayState(GAMESINGLESTATE));
+		this.addState(new GamePlayState(GAMEMULTISTATE));
+		this.addState(new InstructionState(INSTRUCTIONSTATE));
+		this.addState(new HighscoreState(HIGHSCORESTATE));
+	}
+
+	public static int screenX() {
+		return screenX;
+	}
+
+	public static int screenY() {
+		return screenY;
+	}
 
 }
--- src/PongBall.java
+++ src/PongBall.java
@@ -53,4 +53,5 @@ public class PongBall extends MoveableObject {
 			}
 		}
 	}
+
 }
